70fa2db24ed0463bc4087958f19d10fde320ff79
1thiagoCRUZ <thiagocruz58452@gmail.com>
2023-08-29 17:10:02 -0300

    Banner

--- unimar/js/sidebar.js
+++ unimar/js/sidebar.js
@@ -5,6 +5,11 @@ const body = document.querySelector("body"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
+//Carousel
+const bullets = document.querySelectorAll(".bullets-carousel span");
+const images = document.querySelectorAll(".image");
+////////
+
       toggle.addEventListener("click", () => {
         sidebar.classList.toggle("close");
       });
@@ -23,3 +28,20 @@ const body = document.querySelector("body"),
         }
       });
 
+
+function moveSlider() {
+  let index = this.dataset.value;
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach(img => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach(bull => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach(bullet => {
+  bullet.addEventListener("click", moveSlider);
+})
